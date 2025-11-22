0c8611ab5a8bfa2cbee9166bb29c8a72f7181cd0
logisoltech <operations@logisol.tech>
2025-11-22 05:33:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://hitek.vercel.app',
+      'https://hitek.vercel.app/',
+      'http://localhost:3000'
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
